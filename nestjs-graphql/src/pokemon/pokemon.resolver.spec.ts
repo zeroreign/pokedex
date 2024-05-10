8c1182ace2ac662c9811f90f5ea3cb6865a79896
Eddie <develop.ed.lam@gmail.com>
2024-05-10 21:08:19 +0800

    test: Unit tests for resolver. This is to compare it to the e2e test (System Integration Test)

--- nestjs-graphql/src/pokemon/pokemon.resolver.spec.ts
+++ nestjs-graphql/src/pokemon/pokemon.resolver.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PokemonResolver } from './pokemon.resolver';
+import { PokemonService } from './pokemon.service';
 
 describe('PokemonResolver', () => {
   let resolver: PokemonResolver;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PokemonResolver],
+      providers: [PokemonResolver, PokemonService],
     }).compile();
 
     resolver = module.get<PokemonResolver>(PokemonResolver);
@@ -17,12 +18,14 @@ describe('PokemonResolver', () => {
   });
 
   it('should be able to get a pokemon from a service', async () => {
-    const pokemon = await resolver.getPokemon({ name: 'bulbasaur' });
-    expect(pokemon).toBeDefined();
+    const pokemonByName = await resolver.pokemon({name: 'bulbasaur'});
+    const pokemonById = await resolver.pokemon({id: 1});
+    expect(pokemonByName).toBeTruthy();
+    expect(pokemonById).toBeTruthy();
   });
 
   it('should be able to return pokemon types', async () => {
       const response = await resolver.types();
-      expect(response).toBeDefined();
+      expect(response).toBeTruthy();
     });
 });
