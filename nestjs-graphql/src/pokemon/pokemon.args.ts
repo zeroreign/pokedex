46fb90b5184e328398acb06a0e6943d782f4e5ac
Eddie <develop.ed.lam@gmail.com>
2024-05-10 21:23:18 +0800

    feat: Use Validation Pipes and validate query args

--- nestjs-graphql/src/pokemon/pokemon.args.ts
+++ nestjs-graphql/src/pokemon/pokemon.args.ts
@@ -1,11 +1,21 @@
 import { ArgsType, Field } from "@nestjs/graphql";
+import { IsNotEmpty, IsString, Min, ValidateIf } from "class-validator";
+import { IsOnlyQueryArg } from "./pokemon.validators";
 
 @ArgsType()
 export class GetPokemonQueryArgs {
   @Field({ nullable: true })
+  @ValidateIf((o, value) => o.id === undefined || o.id === null)
+  @IsOnlyQueryArg({message: 'Query by name or ID, but not both'})
+  @IsNotEmpty({message: 'A Name or ID is required, but none was provided'})
+  @IsString({message: 'Name must be a string'})
   name?: string;
 
   @Field({ nullable: true })
+  @ValidateIf((o, value) => o.name === undefined || o.name === null)
+  @IsOnlyQueryArg({message: 'Query by name or ID, but not both'})
+  @IsNotEmpty({message: 'A Name or ID is required, but none was provided'})
+  @Min(1, {message: 'There is no pokemon with an ID less than 1'})
   id?: number;
 
 }
---
+++ nestjs-graphql/src/pokemon/pokemon.validators.ts
@@ -0,0 +1,23 @@
+import { registerDecorator, ValidationOptions, ValidationArguments } from 'class-validator';
+
+/**
+ * Validates that the property is the only query argument
+ * @param validationOptions 
+ * @returns true if property is the only argument
+ */
+export function IsOnlyQueryArg(validationOptions?: ValidationOptions) {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      name: 'isOnlyQueryArg',
+      target: object.constructor,
+      propertyName: propertyName,
+      constraints: [],
+      options: validationOptions,
+      validator: {
+        validate(value: any, args: ValidationArguments) {
+          return Object.values(args.object).length === 1;
+        },
+      },
+    });
+  };
+}
