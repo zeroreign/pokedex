564acaf13504c2678a2dceef6987648262d1b9f9
Eddie <develop.ed.lam@gmail.com>
2024-05-16 09:00:10 +0800

    test: Missing space and snapshot was ignored before

--- frontend/src/components/PokemonDetails/PokemonDetails.tsx
+++ frontend/src/components/PokemonDetails/PokemonDetails.tsx
@@ -6,7 +6,7 @@ const PokemonDetails = ({ details, className }: PokemonDetailsProps) => {
   return (
     <div className={"bg-brown text-white font-mono tracking-wider relative"}>
       <div className="flex justify-between">
-        <h2>#{details.id}</h2>
+        <h2># {details.id}</h2>
         <h2>{capitalize(details.name)}</h2>
       </div>
       <ul className="flex justify-evenly">
