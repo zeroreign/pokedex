38cc10e9303f0acf54a21a8a2bc0e88248c5aacb
Eddie <develop.ed.lam@gmail.com>
2024-05-17 13:59:57 +0800

    test: Reorganise test by behaviour
    This is starting to look implementation detaily but necessary to set boundary around the component.

--- frontend/src/components/FramedScreen/index.spec.tsx
+++ frontend/src/components/FramedScreen/index.spec.tsx
@@ -9,39 +9,110 @@ describe("Screen Frame", () => {
     expect(screen.queryByRole("img")).not.toBeInTheDocument();
   });
 
-  it("should display the pokemon image", async () => {
-    const pokemon: Pokemon = {
-      name: "gengar",
-      image:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/94.svg",
-    } as const;
-    render(<FramedScreen pokemon={pokemon} />);
-    const pokemonImageElement = screen.getByRole("img", { name: pokemon.name });
-    expect(pokemonImageElement).toBeInTheDocument();
-    expect(pokemonImageElement).toHaveAttribute("src", pokemon.image);
+  describe("Props", () => {
+    it("should display the pokemon image", async () => {
+      const pokemon: Pokemon = {
+        name: "gengar",
+        image:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/94.svg",
+      } as const;
+      render(<FramedScreen pokemon={pokemon} />);
+      const pokemonImageElement = screen.getByRole("img", {
+        name: pokemon.name,
+      });
+      expect(pokemonImageElement).toBeInTheDocument();
+      expect(pokemonImageElement).toHaveAttribute("src", pokemon.image);
+    });
+
+    it("should set the component default max width and height when not available", async () => {
+      const pokemon: Pokemon = {
+        name: "gengar",
+        image:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/94.svg",
+      } as const;
+      render(<FramedScreen pokemon={pokemon} />);
+      const pokemonImageElement = screen.getByRole("img", {
+        name: pokemon.name,
+      });
+      expect(pokemonImageElement).toBeInTheDocument();
+      expect(pokemonImageElement).toHaveAttribute("src", pokemon.image);
+      expect(pokemonImageElement).toHaveClass("max-w-5");
+      expect(pokemonImageElement).toHaveClass("max-h-5");
+    });
+
+    it("should set the component max width when passed", async () => {
+      const pokemon: Pokemon = {
+        name: "gengar",
+        image:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/94.svg",
+      } as const;
+      render(<FramedScreen pokemon={pokemon} width={10}/>);
+      const pokemonImageElement = screen.getByRole("img", {
+        name: pokemon.name,
+      });
+      screen.debug();
+      expect(pokemonImageElement).toBeInTheDocument();
+      expect(pokemonImageElement).toHaveAttribute("src", pokemon.image);
+      expect(pokemonImageElement).toHaveClass("max-w-10");
+      expect(pokemonImageElement).toHaveClass("max-h-5");
+    });
+
+    it("should set the component max height when passed", async () => {
+      const pokemon: Pokemon = {
+        name: "gengar",
+        image:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/94.svg",
+      } as const;
+      render(<FramedScreen pokemon={pokemon} height={10} />);
+      const pokemonImageElement = screen.getByRole("img", {
+        name: pokemon.name,
+      });
+      expect(pokemonImageElement).toBeInTheDocument();
+      expect(pokemonImageElement).toHaveAttribute("src", pokemon.image);
+      expect(pokemonImageElement).toHaveClass("max-w-5");
+      expect(pokemonImageElement).toHaveClass("max-h-10");
+    });
+
+    it("should set the component max width and height when passed", async () => {
+      const pokemon: Pokemon = {
+        name: "gengar",
+        image:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/94.svg",
+      } as const;
+      render(<FramedScreen pokemon={pokemon} width={10} height={15} />);
+      const pokemonImageElement = screen.getByRole("img", {
+        name: pokemon.name,
+      });
+      expect(pokemonImageElement).toBeInTheDocument();
+      expect(pokemonImageElement).toHaveAttribute("src", pokemon.image);
+      expect(pokemonImageElement).toHaveClass("max-w-10");
+      expect(pokemonImageElement).toHaveClass("max-h-15");
+    });
   });
 
-  it("should show error if uri is invalid", async () => {
-    const pokemon: Pokemon = {
-      name: "gengar",
-      image:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/invalid-uri-error.svg",
-    } as const;
-    render(<FramedScreen pokemon={pokemon} />);
-    const pokemonImageElement = screen.queryByRole("img", {
-      name: pokemon.name,
-    });
-    fireEvent.error(pokemonImageElement as Element); //Testing Library limitation requires event to be fired manually because URL was not fetched
-
-    await waitFor(() => {
-      const errorImageElement = screen.queryByRole("img", {
-        name: `Failed to load image ${pokemon.name}`,
+  describe("Error handling", () => {
+    it("should show error image if uri is invalid", async () => {
+      const pokemon: Pokemon = {
+        name: "gengar",
+        image:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/invalid-uri-error.svg",
+      } as const;
+      render(<FramedScreen pokemon={pokemon} />);
+      const pokemonImageElement = screen.queryByRole("img", {
+        name: pokemon.name,
       });
-      expect(errorImageElement).toBeInTheDocument();
-      expect(errorImageElement).toHaveAttribute(
-        "src",
-        expect.stringContaining("unown-error.gif")
-      );
+      fireEvent.error(pokemonImageElement as Element); //Testing Library limitation requires event to be fired manually because URL was not fetched
+
+      await waitFor(() => {
+        const errorImageElement = screen.queryByRole("img", {
+          name: `Failed to load image ${pokemon.name}`,
+        });
+        expect(errorImageElement).toBeInTheDocument();
+        expect(errorImageElement).toHaveAttribute(
+          "src",
+          expect.stringContaining("unown-error.gif")
+        );
+      });
     });
   });
 });
