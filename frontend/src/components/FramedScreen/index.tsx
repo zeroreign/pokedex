ecb396db598a892454e0bc6ecbd9ac24ce1c42bf
Eddie <develop.ed.lam@gmail.com>
2024-06-03 11:10:27 +0800

    add: Background

--- frontend/src/components/FramedScreen/index.tsx
+++ frontend/src/components/FramedScreen/index.tsx
@@ -23,7 +23,7 @@ const FramedScreen = ({
   return (
     <div
       id="framed-screen"
-      className={`bg-white relative shadow shadow-[#000000] pt-8 pl-9 pr-7 pb-11 m-2 border-double border-r-4	border-b-4 border-t-[1px] border-l border-[#000000] rounded-bl-[2.5rem] rounded-t-md rounded-br-md aspect-[4/3] w-10/12`}
+      className={`z-20 bg-white relative shadow shadow-[#000000] pt-8 pl-9 pr-7 pb-11 m-2 border-double border-r-4	border-b-4 border-t-[1px] border-l border-[#000000] rounded-bl-[2.5rem] rounded-t-md rounded-br-md aspect-[4/3] w-10/12`}
     >
       <div className="bg-black flex items-center justify-center relative text-white text-wrap rounded-[0.25rem] h-full w-full shadow-border">
         {pokemon?.image && (
@@ -33,7 +33,8 @@ const FramedScreen = ({
             fill={true}
             unoptimized={true}
             onError={handleError}
-            className="object-contain"
+            className="z-50 object-contain"
+            priority={true}
           />
         )}
       </div>
